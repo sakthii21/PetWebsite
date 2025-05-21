ac3aa9912ec011ccb3f6c29846aacf79bf29b7fa
Sakthi <125258052+sakthii21@users.noreply.github.com>
2025-05-21 22:30:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "https://sakthi21petwebsite.netlify.app/", credentials: true }));
+app.use(cors({ origin: "https://sakthi21petwebsite.netlify.app", credentials: true }));
 
 
 // Routes
